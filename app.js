9bc76c4e6d5815897f558ce5b48148c30f52a2d3
IkeAni <ike.aniebonam@hotmail.com>
2025-10-16 11:43:48 +0300

    added deploy.yml and days quote prefix

--- app.js
+++ app.js
@@ -3,15 +3,16 @@ const quotes = require('./quotes');
 
 const app = express();
 
-app.get('/quote',(req,res)=>{
+app.get('/quote', (req, res) => {
     const randomIndex = Math.floor(Math.random() * quotes.length);
-    res.json({quote: quotes[randomIndex]});
+
+    res.json({ quote: "Day's quote: " + quotes[randomIndex] });
 });
 
 const PORT = process.env.PORT || 3000;
 
 if (require.main === module) {
-    app.listen(PORT,()=>{
+    app.listen(PORT, () => {
         console.log(`Server running on port ${PORT}`);
     });
 }
